3ea543f97200179ae08332c769f93be27078d35a
mirvise <54601091+mirvise@users.noreply.github.com>
2024-11-06 16:59:08 -0500

    added via array locally
    changes made

--- index.js
+++ index.js
@@ -22,3 +22,21 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+const urls = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  const shorturl = urls.length + 1;
+  urls[shorturl] = url;
+  res.json({ original_url: url, short_url: shorturl });
+});
+
+app.get('/api/shorturl/:shorturl', function(req, res) {
+  const shorturl = req.params.shorturl;
+  const url = urls[shorturl];
+  res.redirect(url);
+});
